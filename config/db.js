1e1186dce04746df8a1b08f5d48360880506219d
jadewang425 <jadewang425@gmail.com>
2023-11-20 16:55:01 -0500

    connected MongoDB

---
+++ config/db.js
@@ -0,0 +1,13 @@
+'use strict'
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const database = {
+    development: process.env.MONGODB_URI,
+    test: process.env.MONGODB_URI,
+}
+
+const localDb = process.env.TESTENV ? database.test : database.development
+
+export const currentDb = process.env.MONGODB_URI || localDb
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import dotenv from "dotenv"
+import mongoose from 'mongoose'
+import { currentDb } from './config/db.js'
 
 dotenv.config()
 
 const app = express()
+
+mongoose
+    .connect(currentDb)
+    .then(() => console.log('DB connected!'))
+    .catch(err => console.log('DB ERROR =>', err))
 
 app.get('/', (req, res) => {
     res.json({
@@ -16,3 +23,4 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Ecommerce API listening on port ${port}`)
 })
+
