3aef2fb6144af4e55beb44b3727367e0cc8233eb
jadewang425 <jadewang425@gmail.com>
2023-11-29 14:16:17 -0500

    added category update and show routes

--- routes/category.js
+++ routes/category.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 import { requireToken, isAdmin } from '../helper/auth.js'
 
-// POST / create category
+// POST / create
 router.post('/category', requireToken, isAdmin, async (req, res) => {
     try {
         const {name} = req.body
@@ -24,6 +24,50 @@ router.post('/category', requireToken, isAdmin, async (req, res) => {
     } catch (err) {
         console.log(err)
         return res.status(400).json(err)
+    }
+})
+
+// PUT / update
+router.put('/category/:categoryId', requireToken, isAdmin, async (req, res) => {
+    try {
+        const category = await Category.findByIdAndUpdate(req.params.categoryId, {
+            name: req.body.name,
+            slug: slugify(req.body.name)
+        }, {
+            new: true
+        })
+        res.json(category)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err.message)
+    }
+})
+// DELETE / delete
+router.delete('/category/:categoryId', requireToken, isAdmin, async (req, res) => {
+    try {
+        //
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err.message)
+    }
+})
+// GET / index - show list of categories
+router.get('/categories', async (req, res) => {
+    try {
+        //
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err.message)
+    }
+})
+// GET / show a category
+router.get('/category/:slug', async (req, res) => {
+    try {
+        const category = await Category.findOne({slug: req.params.slug})
+        res.json(category)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err.message)
     }
 })
 
