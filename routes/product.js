f53d329d6f4753af63b33bd21f26ed2a8e6b6c4d
jadewang425 <jadewang425@gmail.com>
2023-11-30 13:44:38 -0500

    updated product update controller

--- routes/product.js
+++ routes/product.js
@@ -48,9 +48,40 @@ router.post('/product', requireToken, isAdmin, formidable(), async (req, res) =>
 })
 
 // PUT - update
-router.put('/product/:productId', requireToken, isAdmin, async (req, res) => {
+router.put('/product/:productId', requireToken, isAdmin, formidable(), async (req, res) => {
     try {
-        //
+        const { name, description, price, category, quantity, shipping } = req.fields
+        const { photo } = req.files
+        // validation
+        switch(true) {
+            case !name.trim():
+                res.json({ error: 'Name is required'})
+            case !description.trim():
+                res.json({ error: 'Description is required'})
+            case !price.trim():
+                res.json({ error: 'Price is required'})
+            case !category.trim():
+                res.json({ error: 'Category is required'})
+            case !shipping.trim():
+                res.json({ error: 'Shipping is required'})
+            case photo && photo.size > 1000000:
+                res.json({ error: 'Photo must be less than 1mb in size'})
+        }
+
+        // update product
+        const product = await Product.findByIdAndUpdate(
+            req.params.productId, 
+            { ...req.fields, slug: slugify(name), },
+            { new: true }
+        )
+
+        if (photo) {
+            product.photo.data = fs.readFileSync(photo.path)
+            product.photo.contentType = photo.type
+        }
+        await product.save()
+        res.json(product)
+
     } catch (err) {
         console.log(err)
         return res.status(400).json(err)
@@ -60,7 +91,7 @@ router.put('/product/:productId', requireToken, isAdmin, async (req, res) => {
 // DELETE
 router.delete('/product/:productId', requireToken, isAdmin, async (req, res) => {
     try {
-        const removed = await Product.findByIdAndDelete(req.params.productId)
+        const removed = await Product.findByIdAndDelete(req.params.productId).select("-photo")
         res.json(removed)
     } catch (err) {
         console.log(err)
@@ -82,6 +113,20 @@ router.get('/product/:productId', async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId).populate('category').select("-photo")
         res.json(product)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err)
+    }
+})
+
+// photo route
+router.get('/product/photo/:productId', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.productId).select("photo")
+        if(product.photo.data) {
+            res.set('Content-Type', product.photo.contentType)
+            return res.send(product.photo.data)
+        }
     } catch (err) {
         console.log(err)
         return res.status(400).json(err)
